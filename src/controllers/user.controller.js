e4744cae30eca72d5591459c415eab11f9911fa6
KrishnanshPuri <krishnanshpuri@gmail.com>
2026-04-20 23:02:52 +0530

    Auth Done Logout Done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -132,4 +132,27 @@ const loginUser = asyncHandler(async(req,res)=>{
 
 })
 
-export {registerUser}
+const logoutUser = asyncHandler(async(req,res)=>{
+    User.findByIdAndUpdate(req.user._id,
+        {
+            $set:{
+                refreshToken:undefined
+            }
+        },
+        {
+            new:true
+        }
+    )
+    const options={
+        httpOnly:true,
+        secure:true
+    }
+
+    return res
+            .status(200)
+            .clearCookie("accessToken",options)
+            .clearCookie("refreshToken",options)
+            .json(new ApiResponse(200,{},"user logged Out"))
+})
+
+export {registerUser,loginUser,logoutUser}
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,8 +1,26 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/APIError.js";
 import jwt from "jsonwebtoken"
-export const verifyJWT = asyncHandler((req,res,next)=>{
-    const token = req.cokkies?.accessToken||req.header("Authorization")?.replace("Bearer ","")
-    
+import { User } from "../models/user.model.js";
+export const verifyJWT = asyncHandler(async (req,res,next)=>{
+   try {
+     const token = req.cokkies?.accessToken||req.header("Authorization")?.replace("Bearer ","")
+     if(!token){
+         throw new ApiError(401,"Unauthorised Access")
+     }
+     
+     const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+ 
+     const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+ 
+     if(!user){
+         throw new ApiError(401,"Invalid Access Token")
+     }
+ 
+     req.user=user;
+     next();
+   } catch (error) {
+        throw new ApiError(401,error.message || "Invalid Bro");
+   }
 })
 
